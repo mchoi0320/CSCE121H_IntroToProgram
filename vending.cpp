b2dda590848e93480536c6a1a048676d188b335b
MIN CHOI <mchoi0320@compute.cs.tamu.edu>
2019-10-04 19:36:47 -0500

    vending machine updates

--- vending.cpp
+++ vending.cpp
@@ -40,16 +40,97 @@ double addBalance(string n)
 		return 0.10; }
 	else {
 		return 0.05; }
-} 
+}
 
-double getChange(double n)
+double getChange(double balance, vector<string> & contents, vector<string> & change)
 {
-	if (n-0.25>=-0.01) {
-		return 0.25; }
-	else if (n-0.10>=-0.01) {
-		return 0.10; }
+	int posQuarter = itemPosition(contents, "quarter"); int numQuarters = numItems(contents, posQuarter);
+	int posDime = itemPosition(contents, "dime"); int numDimes = numItems(contents, posDime);
+	int posNickel = itemPosition(contents, "nickel"); int numNickels = numItems(contents, posNickel);
+	
+	double n = balance;
+	
+/* 	int a = 0;
+	while(numQuarters > 0 && n - .25 >= 0)
+	{
+		change.push_back("quarter");
+		n -= .25;
+		a++;
+		numQuarters--;
+		cout << n << endl;
+	}
+	int b = 0;
+	cout << numDimes << endl;
+	while(numDimes > 0 && n - .10 >= 0)
+	{
+		change.push_back("dime");
+		n -= .10;
+		b++;
+		numDimes--;
+		cout << n << endl;
+	}
+	int c = 0;
+	while(numNickels > 0 && n - .05 >= 0)
+	{
+		change.push_back("nickel");
+		n -= .05;
+		c++;
+		numNickels;
+		cout << n << endl;
+	}
+
+	cout << n << endl;
+
+	if(n>0.001)
+	{
+		cout << "Insufficent change.\n";
+	}
+	else
+	{
+		cout << "[Returned: ";
+		for(int i = 0; i<change.size()-1; i++)
+		{
+			cout << change[i] << " ";
+		}
+		cout << change[change.size()-1] << "]\n";
+	}
+
+	printVector_str(contents); */
+
+	int numQChange = n / 0.25; int a=0;
+	for (int i=0; i<numQChange; i++) {
+		if (numQuarters > 0) {
+			change.push_back("quarter");
+			a++;
+			/* numQuarters--; */ } }
+	n -= 0.25 * a;
+	int numDChange = n / 0.10; int b=0;
+	for (int i=0; i<numDChange; i++) {
+		if (numDimes > 0) {
+			change.push_back("dime");
+			b++;
+			/* numDimes--; */ } } 
+	n -= 0.10 * b;
+	int numNChange = n / 0.05; int c=0;
+	for (int i=0; i<numNChange; i++) {
+		if (numNickels > 0) {
+			change.push_back("nickel");
+			c++;
+			/* numNickels--; */ } } 
+	n -= 0.05 * c;
+	
+	if (n > 0.001) {
+		cout << "[Insufficient change in machine.]" << endl;
+		return balance; }
 	else {
-		return 0.05; }
+		cout << "[Returned: ";
+		for (int i=0; i<change.size()-1; i++) {
+			cout << change.at(i) << ", "; }
+		cout << change.at(change.size()-1) << ']' << endl;
+		numQuarters -= a; updateContents(contents, posQuarter+1, to_string(numQuarters));
+		numDimes -= b; updateContents(contents, posDime+1, to_string(numDimes));
+		numNickels -= c; updateContents(contents, posNickel+1, to_string(numNickels));
+		return 0; }
 }
 
 
@@ -62,16 +143,10 @@ int main(int argc, char** argv)
 	ifstream file;
 	file.open(argv[1]);
 
-	//vector<string> coins; vector<int> coinsCnt; vector<double> coinsVal;
-	//vector<string> items; vector<int> itemsCnt; vector<double> itemsVal;
 	string fileInput;
 	vector<string> contents;
 	while (file >> fileInput) {
 		contents.push_back(fileInput); }
-
-	/*for (int i=0; i<contents.size(); i++) {
-		cout << contents.at(i) << " "; }
-	cout << endl;*/
 
 	cout << "\nWelcome to Choi Vending!" << endl;
 	cout << "----------------------------------------------------------" << endl;
@@ -81,92 +156,18 @@ int main(int argc, char** argv)
 	cout << "Enter \"quit\" to exit." << endl;
 	cout << "----------------------------------------------------------" << endl;
 
-	/* cout << setw(15) << "\nItems" << setw(15) << "Prices" << endl;
-	for (int i=0; i<contents.size(); i++) {
+/* 	cout << setw(15) << "\nItems" << setw(15) << "Prices" << endl;
+	for (int i=0; i<contents.size()/3; i++) {
 		cout << setw(15) << contents.at(i) } */
 
 	double balance=0;
-	//vector<string> inputVals;
 
 	cout << fixed << setprecision(2);
 
-	//string input;
-
 	cout << "Balance: $" << balance << endl; cout << endl;
-	cout << ">> "; //cin >> input;
+	cout << ">> "; 
 	
-	/*getline(cin,input);
-	if (input.find("press")) {
-		cout << "coins and item" << endl;
-		cout << input.find("press") << endl;
-		//int i;
-		//for (i=0; i<; i++) {
-			 } //}
-	else {
-		cout << "just coins" << endl; }
-*/
 	string input; string indivInput;
-
-/*	getline(cin, input);
-	istringstream ss(input);
-	while (ss >> indivInput) {
-		cout << indivInput << endl; */
-		
-		/*if (indivInput == ("quarter" || "dime" || "nickel") {
-			coins.push_back(indivInput); } } */
-
-	//cin >> input;
-	/* while (getline(cin, input)) {
-		vector<string> inputs;
-		giveInput(inputs, input);
-	//printVector_str(inputs);
-	//istringstream ss(input);
-	//while (ss >> indivInput) {
-		//cout << indivInput << endl;
-		for (int i=0; i<inputs.size(); i++) {
-			string currentInput = inputs.at(i);
-			while (currentInput != "quit") {
-				if (currentInput == "quarter") {
-					balance += 0.25; }
-				else if (currentInput == "dime") {
-					balance += 0.10; }
-				else if (currentInput == "nickel") {
-					balance += 0.05; }
-		//	else {
-		//		cout << "Uh oh! Please enter a valid coin or command." << endl; } }
-		//cout << "Balance: $" << balance << endl; cout << endl;
-		//cout << ">> "; cin >> input; }
-				else if (currentInput == "change") {
-					double changeGiven; double remBalance=balance;
-					vector<string> change;
-					while (remBalance > 0.01) {
-						changeGiven = getChange(remBalance);
-						if (changeGiven == 0.25) {
-							change.push_back("quarter"); }
-						else if (changeGiven == 0.10) {
-							change.push_back("dime"); }
-						else {
-							change.push_back("nickel"); }
-					remBalance -= changeGiven;
-					balance = 0;
-					cout << remBalance << endl; }
-					for (int i=0; i<change.size(); i++){
-						cout << change.at(i) << " "; }
-					cout << endl; }
-				else if (currentInput == "press") {
-					cout << "Nothing done yet" << endl; }
-				else {
-					cout << "Uh oh! Please enter a valid coin or command." << endl; } }
-		//istringstream ss(input); }
-			cout << "Balance: $" << balance << endl; cout << endl;
-			cout << ">> "; //cin >> indivInput; 
-		//ss.clear();
-			getline(cin, input);
-			//ss >> indivInput;
-			vector<string> inputs;
-			giveInput(inputs, input); } } */
-
-	//cout << input << endl;*/
 	
 	getline(cin, input);
 	while (input.find("quit") == string::npos) {
@@ -185,130 +186,42 @@ int main(int argc, char** argv)
 					int itemNum = numItems(contents, itemPos);
 					itemNum++;
 					updateContents(contents, itemPos+1, to_string(itemNum)); } }
-			/* if (currentInput == "quarter") {
-				balance += 0.25; }
-			else if (currentInput == "dime") {
-				balance += 0.10; }
-			else if (currentInput == "nickel") {
-				balance += 0.05; } */
 			else if (currentInput == "change") {
-				if (balance < 0.05) cout << "No outstanding balance." << endl;
+				if (balance < 0.05) cout << "[No outstanding balance.]" << endl;
 				else {
-					double changeGiven; double remBalance=balance;
 					vector<string> change;
-					string changeIs;
-					while (remBalance > 0.01) {
-						changeGiven = getChange(remBalance);
-						if (changeGiven == 0.25) changeIs = "quarter";
-						else if (changeGiven == 0.10) changeIs = "dime";
-						else changeIs = "nickel";
-						change.push_back(changeIs);
-						remBalance -= changeGiven; }
-						/* if (changeGiven == 0.25) {
-							change.push_back("quarter"); }
-						else if (changeGiven == 0.10) {
-							change.push_back("dime"); }
-						else {
-							change.push_back("nickel"); }
-						remBalance -= changeGiven; } */
-						/* int itemPos = itemPosition(contents, changeIs);
-						int itemNum = numItems(contents, itemPos);
-						if (itemNum > 0) {
-							itemNum--;
-							updateContents(contents, itemPos+1, to_string(itemNum));
-							if (changeGiven == 0.25) {
-								change.push_back("quarter"); }
-							else if (changeGiven == 0.10) {
-								change.push_back("dime"); }
-							else {
-								change.push_back("nickel"); } }
-						else {
-							cout << "insufficient change" << endl;
-							changeGiven = getChange(remBalance);
-							if (changeIs == "quarter") {
-								if (n-0.10>=-0.01) {
-									change.push_back("dime"); }
-								else {
-									change.push_back("nickel"); } }
-							else if (changeIs == "dime") {
-								if (n-0.05>=-0.01) {
-									change.push_back("nickel"); } }
-							}
-						remBalance -= changeGiven; } */
-					balance = 0;
-					for (int i=0; i<change.size(); i++) {
-						/* int itemPos = itemPosition(contents, change.at(i));
-						int itemNum = numItems(contents, itemPos); 
-						if (itemNum > 0) {
-							itemNum--;
-							updateContents(contents, itemPos+1, to_string(itemNum); } */
-						cout << change.at(i) << " "; } }
-				cout << endl; }
+					balance = getChange(balance, contents, change); } }
 			else if (currentInput == "press") {
 				string item = inputs.at(i+1);
 				if (item == "quarter" || item == "dime" || item == "nickel") {
-					cout << "Please choose a valid item." << endl; }
+					cout << "[Please choose a valid item.]" << endl; }
 				else {
 					int itemPos = itemPosition(contents, item);
-					/* for (int i=0; i<contents.size(); i++) {
-						if (contents.at(i) == item) itemPos = i; } */
-					/* int itemNum = stoi(contents.at(itemPos+1)); */
 					int itemNum = numItems(contents, itemPos);
 					float itemVal = stof(contents.at(itemPos+2));				
 					if (itemNum > 0 && balance >= itemVal) {
-						cout << "Dispensed " << item << '.' << endl;
+						cout << "[Dispensed: " << item << ']' << endl;
 						balance -= itemVal;
-						itemNum--; /* contents.at(itemPos+1) = to_string(itemNum); */ updateContents(contents, itemPos+1, to_string(itemNum)); }
+						itemNum--; updateContents(contents, itemPos+1, to_string(itemNum)); }
 					else if (balance < itemVal) {
-						cout << "Not enough balance." << endl; }
+						cout << "[Not enough balance.]" << endl; }
 					else if (itemNum == 0) {
-						cout << item << " is out of stock." << endl; } } }
+						item[0] = toupper(item[0]);
+						cout << '[' << item << " is out of stock.]" << endl; } } }
 			else {
 				if (i > 0 && inputs.at(i-1) == "press") continue;
 				else {
-					cout << "Uh oh! Please enter a valid coin or command." << endl; } } }
+					cout << "[Uh oh! Please enter a valid coin or command.]" << endl; } } }
 		cout << "Balance: $" << balance << endl; cout << endl;
 		cout << ">> "; getline(cin, input); }
-
-	/* cin >> input;		
-	while (input != "quit") {
-				if (input == "quarter") {
-					balance += 0.25; }
-				else if (input == "dime") {
-					balance += 0.10; }
-				else if (input == "nickel") {
-					balance += 0.05; }
-				if (input == "quarter" || input == "nickel" || input == "dime") {
-					balance += addBalance(input); } */
-		//	else {
-		//		cout << "Uh oh! Please enter a valid coin or command." << endl; } }
-		//cout << "Balance: $" << balance << endl; cout << endl;
-		//cout << ">> "; cin >> input; }
-				/* else if (input == "change") {
-					if (balance < 0.05) cout << "No outstanding balance." << endl;
-					else {
-						double changeGiven; double remBalance=balance;
-						vector<string> change;
-						while (remBalance > 0.01) {
-							changeGiven = getChange(remBalance);
-							if (changeGiven == 0.25) {
-								change.push_back("quarter"); }
-							else if (changeGiven == 0.10) {
-								change.push_back("dime"); }
-							else {
-								change.push_back("nickel"); }
-						remBalance -= changeGiven;
-						balance = 0; }
-						for (int i=0; i<change.size(); i++){
-							cout << change.at(i) << " "; }
-					cout << endl; } }
-				else if (input == "press") {
-					cout << "Nothing done yet" << endl; }
-				else if (input == "show") {
-					cout << "Balance: $" << balance << endl; cout << endl; }
-				else {
-					cout << "Uh oh! Please enter a valid coin or command." << endl; }
-		//istringstream ss(input); }
-			cout << "Balance: $" << balance << endl; cout << endl;
-			cout << ">> "; cin >> input; } */
+		
+	if (balance > 0) {
+		vector<string> change;
+		getChange(balance, contents, change);
+		cout << "Balance: $" << balance << endl;
+		cout << "----------------------------------------------------------" << endl;
+		cout << "Don't forget your change. Thank you!" << endl; }
+	else {
+		cout << "----------------------------------------------------------" << endl;
+		cout << "Thank you!" << endl; }
 }
