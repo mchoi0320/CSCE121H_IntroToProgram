02ff39aac93ba46545a002c2d50d4a9da68996d5
Minji Choi <mchoi0320@compute.cs.tamu.edu>
2019-11-01 15:27:15 -0500

    poly progress

--- poly.cpp
+++ poly.cpp
@@ -22,20 +22,53 @@ vector<string> separate_terms(string polynomial)
 {
 	vector<string> terms;
 	string term;
-	/*istringstream ss(polynomial);
-	string term;
-	while (getline(ss, term, '+')) {
-		terms.push_back(term); }
-*/
-	char delim_plus = '+', delim_minus = '-';
+
 	int term_start=0;
 	for (int i=0; i<polynomial.size(); i++) {
-		if (polynomial[i] == delim_plus || polynomial[i] == delim_minus) {
-			term = polynomial.substr(term_start, i);
+		if ((polynomial[i] == '+' || polynomial[i] == '-') && i != 0) {
+			term = polynomial.substr(term_start, i-term_start);
 			terms.push_back(term);
 			term_start = i; } }
+	string last_term = polynomial.substr(term_start, polynomial.size()-term_start);
+	terms.push_back(last_term);
+
+	if (terms[0].find('+') && terms[0].find('-') == string::npos) {
+		terms[0] = '+' + terms[0]; }
 
 	return terms;
+}
+
+struct TERM {
+	char sign;
+	float coefficient;
+	int exponent;
+};
+
+float get_coefficient(string term)
+{
+	char variable = 'x';
+	int variable_pos = term.find(variable);
+
+	if (variable_pos == 1) return 1;
+	else {
+		string coefficient_str = term.substr(1, variable_pos-1);
+		float coefficient = stof(coefficient_str);
+		return coefficient; }
+}
+
+int get_exponent(string term)
+{
+	if (term.find('^') == string::npos) return 1;
+	else {
+		int exponent_pos = term.find('^') + 1;
+		string exponent_str = term.substr(exponent_pos, term.size());
+		int exponent = stoi(exponent_str);
+		return exponent; }
+}
+
+vector<TERM> get_term_components(vector<string> terms)
+{
+	vector<TERM> terms_components;
 }
 
 int main()
@@ -50,9 +83,19 @@ int main()
 		if (inputs.size() == 1) {
 			print_vec_str(inputs); }
 
-		else {
+		if (inputs.size() == 2) {
+			//code for deriv and root
 			vector<string> terms = separate_terms(inputs[1]);
-			print_vec_str(terms); }
+			//print_vec_str(terms);
+			//cout << get_coefficient(terms[0]) << endl;
+			//cout << get_exponent(terms[0]) << endl;
+			}
+		if (inputs.size() == 3) {
+			vector<string> terms_poly1 = separate_terms(inputs[1]);
+			vector<string> terms_poly2 = separate_terms(inputs[2]);
+			//print_vec_str(terms_poly1);
+			//print_vec_str(terms_poly2);
+			}
 
 		cout << "\n> ";
 		getline(cin, line); }
